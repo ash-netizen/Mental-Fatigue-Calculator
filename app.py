3105915ba311abbfc7e0f1db83023165a4146899
Ashish Pandey <50423343+ash-netizen@users.noreply.github.com>
2021-05-17 10:59:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,12 +20,18 @@ from tensorflow import keras
 predictor = ktrain.load_predictor('predictor')
 
 st.write("This is an application to calculate Employee Mental Fatigue Score")
-image = Image.open("IMG_2605.jpeg")
+image = Image.open("IMG_2605.JPG")
 st.image(image, use_column_width=True)
 
-WFH_Setup_Available =  st.text_input("is work from home enabled for you?")
+WFH_Setup_Available =  st.sidebar.selectbox(
+    'is Work from home enabled for you?',
+    ('Yes', 'No')
+)
 Designation = st.text_input("what is your designation?")
-Company_Type = st.text_input("What is your Company Type?")
+Company_Type = st.sidebar.selectbox(
+    'What is your Company Type?',
+    ('Product', 'Service')
+)
 Average_hours_worked_per_day = st.text_input("how many hours you work on an average per day?")
 Employee_satisfaction_score = st.text_input("Please enter your satisfaction score on scale of 10")
 
@@ -39,11 +45,13 @@ data = pd.DataFrame([data])
 def mental_fatigue_score(WFH_Setup_Available, Designation, Company_Type, Average_hours_worked_per_day, Employee_satisfaction_score):
   prediction = predictor.predict(data)
   if prediction <= 0.3:
-    prediction = 'have poor mental health, please work on it, you can follow  https://www.mhanational.org/31-tips-boost-your-mental-health'
+    prediction = 'Hurrah!, have good Mental Health, keep it up!'
   elif 0.3 < prediction < 0.5:
     prediction = "need to work a bit on your Mental Health, Please follow https://community.virginpulse.com/work-from-home-exercises-to-keep-employees-active-and-healthy"
+  elif 0.5 < prediction < 0.75:
+    prediction = 'have poor Mental health, Please work on it, you can follow  https://www.mhanational.org/31-tips-boost-your-mental-health'
   else:
-    prediction = "have good mental health, keep it up"
+    prediction = 'have serious Mental issues, Please ring a bell and start to take care of your Mental Health, you can start with https://healthblog.uofmhealth.org/health-management/6-ways-to-relieve-your-work-from-home-fatigue'
 
   print(prediction)
   return prediction
@@ -55,10 +63,11 @@ def Score():
   
 
 if st.button("Predict"):
-  result= mental_fatigue_score(WFH_Setup_Available, Designation, Company_Type, Average_hours_worked_per_day, Employee_satisfaction_score)
-  st.success('You {}'.format(result))
-  result1 = Score()
-  st.success('Your Score is {}'.format(int(result1*100)))
+    result1 = Score()
+    st.success('Your Score is {}'.format(int(result1*100)))
+    result= mental_fatigue_score(WFH_Setup_Available, Designation, Company_Type, Average_hours_worked_per_day, Employee_satisfaction_score)
+    st.success('You {}'.format(result))
+  
 
 
 
